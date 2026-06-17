bd9fe20cc148a9415f537f29763f16e1bea54a4d
Soyukke <soyukke@users.noreply.github.com>
2026-06-17 19:26:22 +0900

    selfhost: support depth-2 enum dispatch

--- cmd/kizu/selfhost_parser_closure_test.go
+++ cmd/kizu/selfhost_parser_closure_test.go
@@ -56,6 +56,8 @@ var parserClosureSeeds = []string{
 	"synth_collection_build",
 	// expect_ident exercises !Token ok-wrap and error arms.
 	"expect_ident",
+	// issue 1157 NEW-B fixture: isolates match left.next.kind depth-2 enum dispatch.
+	"synth_depth2_dispatch_match",
 }
 
 var parserClosurePrivateHelpers = []string{
